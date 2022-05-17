f23e0ab871595cd32e07055b4f28c9a224856f5f
Alosra <aleomoeini@gmail.com>
2022-05-17 17:12:07 +0300

    initial code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::path::Path;
+
+fn main() -> std::io::Result<()> {
+    let path = Path::new(".");
+    show_entries(path)
+}
+
+pub fn show_entries(path: &Path) -> std::io::Result<()> {
+    for entry in fs::read_dir(path)? {
+        let entry_path = entry?.path();
+        println!("{:?}", entry_path);
+        if entry_path.is_dir() {
+            show_entries(&entry_path)?;
+        }
+    }
+    Ok(())
 }
